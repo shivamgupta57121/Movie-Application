f4e94d64e92f69c793a101fc2871b509013eb837
shivamgupta57121 <shivamgupta57121@gmail.com>
2021-06-25 23:08:14 +0530

    Movies component

---
+++ src/Components/Movies.js
@@ -0,0 +1,190 @@
+import React, { Component } from 'react'
+// import { getMovies } from './getMovies';
+import axios from 'axios';
+export default class Movies extends Component {
+    constructor() {
+        super();
+        this.state = {
+            movies: [],
+            currSearchText: '',
+            currPage: 1,
+            limit: 4
+        }
+    }
+    async componentDidMount() {
+        console.log("Component Did Mount");
+        let res = await axios.get("https://backend-react-movie.herokuapp.com/movies");
+        console.log(res);
+        this.setState({
+            movies: res.data.movies
+        })
+    }
+    // To update current search text in state
+    handleChange = (e) => {
+        let val = e.target.value;
+        console.log(val);
+        this.setState({
+            currSearchText: val
+        })
+    }
+    // To update limit in state
+    handlePageLimitChange = (e) => {
+        let newLimit = e.target.value;
+        console.log(newLimit);
+        if(newLimit < 1){
+            alert('Limit should be greater than 0.');
+            newLimit = 4;
+        }
+        this.setState({
+            limit : newLimit
+        })
+    }
+    // Delete operation - permanent
+    onDelete = (id) => {
+        let arr = this.state.movies.filter(function (movieObj) {
+            return movieObj._id !== id;
+        })
+        // console.log(arr);
+        this.setState({
+            movies: arr
+        });
+    }
+    // For sorting on basis of Ratings
+    sortByRatings = (e) => {
+        let className = e.target.className;
+        console.log(className);
+        let sortedMovies = [];
+        if (className === 'fa fa-sort-asc') {
+            // ascending order
+            sortedMovies = this.state.movies.sort(function (movieObjA, movieObjB) {
+                return movieObjA.dailyRentalRate - movieObjB.dailyRentalRate;
+            })
+        } else {
+            // descending order
+            sortedMovies = this.state.movies.sort(function (movieObjA, movieObjB) {
+                return movieObjB.dailyRentalRate - movieObjA.dailyRentalRate;
+            })
+        }
+        this.setState({
+            movies: sortedMovies
+        })
+    }
+    // For Sorting on basis of stock
+    sortByStock = (e) => {
+        let className = e.target.className;
+        console.log(className);
+        let sortedMovies = [];
+        if (className === 'fa fa-sort-asc') {
+            // ascending order
+            sortedMovies = this.state.movies.sort(function (movieObjA, movieObjB) {
+                return movieObjA.numberInStock - movieObjB.numberInStock;
+            })
+        } else {
+            // descending order
+            sortedMovies = this.state.movies.sort(function (movieObjA, movieObjB) {
+                return movieObjB.numberInStock - movieObjA.numberInStock;
+            })
+        }
+        this.setState({
+            movies: sortedMovies
+        })
+    }
+    handlePageChange = (pageNumber) => {
+        this.setState({ currPage: pageNumber });
+    }
+    render() {
+        console.log('render');
+        let { movies, currSearchText, currPage, limit } = this.state; //ES6 destructuring
+        let filteredArr = [];
+        // For searching
+        if (currSearchText === '') {
+            filteredArr = movies;
+        }
+        else {
+            filteredArr = movies.filter(function (movieObj) {
+                let title = movieObj.title.trim().toLowerCase();
+                // console.log(title);
+                return title.includes(currSearchText.toLowerCase());
+            })
+        }
+        // For Pagination
+        let numberOfPage = Math.ceil(filteredArr.length / limit);
+        let pageNumberArr = [];
+        for (let i = 0; i < numberOfPage; i++) {
+            pageNumberArr.push(i + 1);
+        }
+        let si = (currPage - 1) * limit;
+        let ei = si + limit;
+        filteredArr = filteredArr.slice(si, ei);
+        // To reset page to first if all elements deleted from last page
+        // if(filteredArr.length === 0){
+        //     this.setState({currPage : 1});
+        // }
+        return (
+            //JSX
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-3'>
+                        Hello
+                    </div>
+                    <div className='col-9'>
+                        <input type='search' value={this.state.currSearchText} onChange={this.handleChange} ></input>
+                        <input type='number' value={this.state.limit} onChange={this.handlePageLimitChange} ></input>
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Title</th>
+                                    <th scope="col">Genre</th>
+                                    <th scope="col">
+                                        <i onClick={this.sortByStock} className="fa fa-sort-asc" aria-hidden="true"></i>
+                                        Stock
+                                        <i onClick={this.sortByStock} className="fa fa-sort-desc" aria-hidden="true"></i>
+                                    </th>
+                                    <th scope="col">
+                                        <i onClick={this.sortByRatings} className="fa fa-sort-asc" aria-hidden="true"></i>
+                                        Rate
+                                        <i onClick={this.sortByRatings} className="fa fa-sort-desc" aria-hidden="true"></i>
+                                    </th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    filteredArr.map((movieObj) => {
+                                        return (
+                                            <tr key={movieObj._id} >
+                                                <td></td>
+                                                <td>{movieObj.title}</td>
+                                                <td>{movieObj.genre.name}</td>
+                                                <td>{movieObj.numberInStock}</td>
+                                                <td>{movieObj.dailyRentalRate}</td>
+                                                <td><button onClick={() => {
+                                                    this.onDelete(movieObj._id)
+                                                }} type="button" className="btn btn-danger">Delete</button></td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                        <nav aria-label="...">
+                            <ul className="pagination">
+                                {
+                                    pageNumberArr.map((pageNumber) => {
+                                        let classStyle = pageNumber === currPage ? 'page-item active' : 'page-item';
+                                        return (
+                                            <li key={pageNumber} onClick={() => this.handlePageChange(pageNumber)} className={classStyle}>
+                                                <span className="page-link">{pageNumber}</span>
+                                            </li>
+                                        )
+                                    })
+                                }
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
